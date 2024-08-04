b5d810b36ad744ebdd407650dbf8b2167c6acea4
Robert Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2024-08-04 15:44:14 +0100

    Commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using static Utils;
 
@@ -9,7 +10,7 @@ public class PlayerController : MonoBehaviour {
     [SerializeField] private float radius = 0.5f;
     [SerializeField] private float movementSpeed = 2.5f;
     [SerializeField] private float rotationSpeed = 60f;
-    
+
     private Rigidbody2D rb;
 
     private float forwardInput;
@@ -35,8 +36,36 @@ public class PlayerController : MonoBehaviour {
     private void FixedUpdate() {
         InputUpdate();
         
-        rb.position += ((Vector2) transform.up * (forwardInput * movementSpeed * Time.fixedDeltaTime)) 
-                            + persistentVelocity;
+        var move_delta = ((Vector2) transform.up * (forwardInput * movementSpeed * Time.fixedDeltaTime))
+                         + persistentVelocity;
+
+        var distance = move_delta.magnitude;
+        if (distance > 0f) {
+            var direction = move_delta.normalized;
+            var hit = Physics2D.CircleCast(transform.position, radius, direction,
+                distance, collisionLayers);
+            if (hit) {
+                var delta = hit.centroid - rb.position;
+                var new_position = delta.normalized * (delta.magnitude * 0.999f);
+                var remaining = distance - (new_position - rb.position).magnitude;
+
+                var new_direction = (direction * (Vector2.one - hit.normal.Abs())).normalized;
+                
+                var hit2 = Physics2D.CircleCast(new_position, radius, new_direction,
+                    remaining, collisionLayers);
+
+                if (hit2) {
+                    rb.position = hit2.centroid;
+                }
+                else {
+                    rb.position = new_position + (new_direction * remaining);
+                }
+            }
+            else {
+                rb.position += move_delta;
+            }
+        }
+
 
         rb.rotation += rotationInput * rotationSpeed * Time.fixedDeltaTime;
     }
@@ -45,8 +74,8 @@ public class PlayerController : MonoBehaviour {
         if (!ContainsLayer(collisionLayers, other.gameObject.layer)) return;
         
         var contact_point = other.GetContact(0);
-        rb.position = contact_point.point + contact_point.normal * (radius + 0.0001f);
+        rb.position = contact_point.point + contact_point.normal * (radius + 0.00001f);
 
-        persistentVelocity *= Vector2.one - contact_point.normal;
+        persistentVelocity *= (Vector2.one - contact_point.normal.Abs()).normalized;
     }
 }
