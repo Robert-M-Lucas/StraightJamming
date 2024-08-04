4e660f057ccf3305e5908698470bef160bef4d2e
Robert Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2024-08-04 11:29:33 +0100

    PUSH

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField] private Rigidbody2D rb;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void JumpHandler(InputAction.CallbackContext context) {
+        rb.AddForce(Vector2.up * 4);
+    }
+}
